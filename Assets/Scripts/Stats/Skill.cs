2ee666a08ba1a11b745684c98857c3d45c49c58a
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-03-09 20:58:01 +0100

    Added Skill class.

---
+++ Assets/Scripts/Stats/Skill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill {
+
+	private StatsEnums.Attribute attribute;
+	private StatsEnums.Skill type;
+	private int value;
+
+	public StatsEnums.Attribute Attribute { get { return attribute; } }
+	public StatsEnums.Skill Type { get { return type; } }
+	public int Value { get { return value; } }
+
+	public Skill(int value, StatsEnums.Skill type, StatsEnums.Attribute attribute) {
+		this.value = value;
+		this.type = type;
+		this.attribute = attribute;
+	}
+
+	public void IncreaseBy(int value) {
+		this.value += value;
+	}
+}
