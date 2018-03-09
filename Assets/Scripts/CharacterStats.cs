d28be9679866ec61d6c87c380ca83dcc7cfd9b66
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-03-09 20:18:22 +0100

    Added initial logic to CharacterStats.

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour {
 
+	private BasicStats basicStats;
+	private Stats stats;
+
 	// Use this for initialization
 	void Start () {
-		
+		basicStats = new BasicStats ("John Doe");
+		stats = new Stats ();
+
+		Debug.Log ("Name: " + basicStats.Name);
+		Debug.Log ("Speed: " + basicStats.Speed);
+		Debug.Log ("Actions: " + basicStats.Actions);
 	}
 	
 	// Update is called once per frame
