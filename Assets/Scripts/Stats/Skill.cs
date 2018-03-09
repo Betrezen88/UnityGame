f2c7bc2b9280aa1aeec71f6b2d236232459674e8
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-03-09 21:07:50 +0100

    Refactor Skill class, remove type, added DecreaseBy method.

--- Assets/Scripts/Stats/Skill.cs
+++ Assets/Scripts/Stats/Skill.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class Skill {
 
 	private StatsEnums.Attribute attribute;
-	private StatsEnums.Skill type;
 	private int value;
 
 	public StatsEnums.Attribute Attribute { get { return attribute; } }
-	public StatsEnums.Skill Type { get { return type; } }
 	public int Value { get { return value; } }
 
-	public Skill(int value, StatsEnums.Skill type, StatsEnums.Attribute attribute) {
+	public Skill(int value, StatsEnums.Attribute attribute) {
 		this.value = value;
-		this.type = type;
 		this.attribute = attribute;
 	}
 
 	public void IncreaseBy(int value) {
 		this.value += value;
+	}
+
+	public void DecreaseBy(int value) {
+		this.value -= value;
 	}
 }
