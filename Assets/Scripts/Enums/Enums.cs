f55081555e0c6bf5e6bf099001b2dfc00ceffea8
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-03-09 20:34:25 +0100

    Added Skill enum.

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -11,4 +11,38 @@ namespace StatsEnums {
 		CHARISMA
 	}
 
+	public enum Skill {
+		// Condition
+		Brawl,
+		HandWeapon,
+		HeavyGuns,
+		Throwing,
+		Muscles,
+		Stealth,
+
+		//Perception
+		Pistols,
+		Guns,
+		Observation,
+		Listening,
+		Survival,
+		Pickpocket,
+
+		// Inteligence
+		Lockpicks,
+		FirstAid,
+		Medicine,
+		Repair,
+		Explosives,
+		Knowladge,
+
+		// Charisma
+		NervesOfSteel,
+		PainResistance,
+		Persuasion,
+		Intimidation,
+		Bluff,
+		Taming
+	}
+
 }
