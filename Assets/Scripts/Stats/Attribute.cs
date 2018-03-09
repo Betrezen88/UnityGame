44cf59bc5d02fbf9407151c01d9d199accd48c4a
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-03-10 00:07:09 +0100

    Add FillSkillList and GetValue methods to Attribute class.

--- Assets/Scripts/Stats/Attribute.cs
+++ Assets/Scripts/Stats/Attribute.cs
@@ -15,5 +15,24 @@ public class Attribute {
 	public Attribute(StatsEnums.Attribute attribute, StatsEnums.AttrType type) {
 		this.attribute = attribute;
 		this.type = type;
+
+		FillSkillList ();
+	}
+
+	private void FillSkillList() {
+		int value = GetValue ();
+		foreach (Skill skill in skills.GetByAttribute(attribute))
+			skill.IncreaseBy (value);
+	}
+
+	private int GetValue() {
+		if (StatsEnums.AttrType.Good == type)
+			return 15;
+		else if (StatsEnums.AttrType.Normal == type)
+			return 10;
+		else if (StatsEnums.AttrType.Bad == type)
+			return 5;
+		else
+			return 0;	// Default, we never get here, this is only for no error purpose.
 	}
 }
