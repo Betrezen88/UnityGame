acf3ef60c92bd1e7083263c58ed478ac6abc3707
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-03-09 23:47:37 +0100

    Added Attribute class.

---
+++ Assets/Scripts/Stats/Attribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attribute {
+
+	private StatsEnums.Attribute attribute;
+	private StatsEnums.AttrType type;
+	private Skills skills = new Skills();
+
+	public StatsEnums.Attribute Attr { get { return attribute; } }
+	public StatsEnums.AttrType Type { get { return type; } }
+	public Skills Skills { get { return skills; } }
+
+	public Attribute(StatsEnums.Attribute attribute, StatsEnums.AttrType type) {
+		this.attribute = attribute;
+		this.type = type;
+	}
+}
