cb486720a11be5f4a9925eda26d70747d5ebca77
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-03-09 19:49:51 +0100

    Added basic logic to BasicStats class.

--- Assets/Scripts/Stats/BasicStats.cs
+++ Assets/Scripts/Stats/BasicStats.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class BasicStats {
 
-	// Name
-	// Speed
-	// Actions
+	private string name;	// Name
+	private float speed;	// Speed
+	private int actions;	// Actions
 
-	public BasicStats() {}
+	public string Name { get { return name; } }
+	public float Speed { get { return speed; } }
+	public int Actions { get { return actions; } }
+
+	public BasicStats(string name) {
+		this.name = name;
+		speed = 3.0f;
+		actions = 3;
+	}
+
+	public BasicStats(string name, float speed, int actions) {
+		this.name = name;
+		this.speed = speed;
+		this.actions = actions;
+	}
 }
