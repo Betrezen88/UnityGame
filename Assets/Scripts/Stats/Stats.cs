3f0cca4976f50a07b021db066f0b0cad46eee1fb
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-03-09 19:35:36 +0100

    Added class Stats.

---
+++ Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats {
+
+	// Attribute list (good, normal, bad)
+
+	// Skills list
+
+	// Characteristic (good, bad)
+
+	// Archetype
+
+	public Stats() {}
+}
